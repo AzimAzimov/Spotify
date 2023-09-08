f551895b5096c479c7e35ec36f27b1be88af46e1
Azim Azimov <mrazim0990@gmail.com>
2023-09-08 19:23:47 +0300

    main

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React, {FC} from 'react';
+
+import LikeButton from "@/components/button/LikeButton";
+import MediaItem from "@/components/song/MediaItem";
+import {Song} from "@/types";
+
+interface ISearchContentProps {
+  songs: Song[]
+}
+
+const SearchContent:FC<ISearchContentProps> = ({songs}) => {
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No songs found
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-y-2 w-full">
+          <div className="flex-1">
+            <MediaItem data={song} onClick={()=> {}} />
+          </div>
+          <LikeButton songId={song.id}/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchContent;
